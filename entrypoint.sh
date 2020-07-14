f7619436c634894eefece09c24d6bbb81830222f
vafinvr <30231588+vafinvr@users.noreply.github.com>
2020-07-15 02:18:46 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,5 +11,5 @@ fi
 while IFS='=' read -r -a var; do
   echo "Setting ${var[0]} to ${var[1]} "
   echo ${var[1]} | wc -l
-  sed -i '' "s|$(printf '%s\n' "\${${var[0]}}" | sed -e 's/[]\/$*.^[]/\\&/g')|${var[1]}|g" $FILENAME
+  sed -i "s|$(printf '%s\n' "\${${var[0]}}" | sed -e 's/[]\/$*.^[]/\\&/g')|${var[1]}|g" $FILENAME
 done < <(printenv)
